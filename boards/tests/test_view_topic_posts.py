0e4a4724e6eb93d7f31f0bfa68a25f3c33e0da37
WONG Ka Hang <wongkahang@WMBP.local>
2019-01-04 22:34:49 +0800

    Initial commit

---
+++ boards/tests/test_view_topic_posts.py
@@ -0,0 +1,35 @@
+# from django.test import TestCase
+
+# Create your tests here.
+# from django.core.urlresolvers import reverse
+
+
+from django.urls import reverse
+from django.test import TestCase
+from django.urls import resolve
+from ..views import new_topic, BoardListView
+from ..models import Board
+from django.contrib.auth.models import User
+from ..views import PostListView
+from ..models import Board, Topic, Post
+from ..forms import NewTopicForm
+
+class TopicPostsTests(TestCase):
+    def setUp(self):
+        board = Board.objects.create(name='Django', description='Django board.')
+        user = User.objects.create_user(username='john', email='john@doe.com', password='123')
+        topic = Topic.objects.create(subject='Hello, world', board=board, starter=user)
+        Post.objects.create(message='Lorem ipsum dolor sit amet', topic=topic, created_by=user)
+        url = reverse('topic_posts', kwargs={'pk': board.pk, 'topic_pk': topic.pk})
+        self.response = self.client.get(url)
+
+    def test_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_view_function(self):
+        view = resolve('/boards/1/topics/1/')
+        self.assertEquals(view.func, topic_posts)
+
+    def test_view_function(self):
+        view = resolve('/boards/1/topics/1/')
+        self.assertEquals(view.func.view_class, PostListView)
